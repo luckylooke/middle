46d50e176683fe6ad5bb8bc3f9e16dd621b71571
Ctibor Laky <luckylooke@gmail.com>
2016-10-23 08:19:12 +0200

    "next" functionality implemented allowing async tasks

--- middle.src.js
+++ middle.src.js
@@ -10,10 +10,10 @@ function Middle(fn){
 }
 
 Middle.prototype.run = function () {
-    var i=0,
-        len = this._stack.length;
-    for(;i<len;i++){
-        this._stack[i].apply(null, arguments);
+    if(this._stack.length){
+        this._stackIndex = 0;
+        this._stackLen = this._stack.length;
+        this.next.apply(this, arguments);
     }
 };
 
@@ -21,12 +21,41 @@ Middle.prototype.use = function (fn) {
     this._stack.push(fn);
 };
 
-/*
-*
-* Use:
- mw = new Middle();
- mw.use(function(){console.log(1);});
- mw.use(function(){console.log(2);});
- mw();
-*
-* */
+Middle.prototype.next = function() {
+    if(this._stackIndex < this._stackLen){
+        this._stackIndex++;
+        this._stack[this._stackIndex - 1]
+            .bind(null, this.next.bind(this))
+            .apply(null, arguments);
+    }
+};
+
+// function argsToArray(argums) {
+//     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Functions/arguments
+//      return (argums.length === 1 ? [argums[0]] : Array.apply(null, argums));
+// }
+
+
+var mw = new Middle(),
+    fn1 = function(next, input){
+        input++;
+        console.log('fn1', input, this.test);
+        next(input);
+    },
+    fn2 = function(next, input){
+        input++;
+        console.log('fn2', input, this.test);
+        setTimeout(function () {
+            next(input); // simulating async task
+        }, 1500);
+    },
+    fn3 = function(next, input){
+        input++;
+        console.log('fn3', input, this.test);
+        next(input);
+    },
+    context = {test: 'red'};
+mw.use(fn1.bind(context));
+mw.use(fn2); // test is undefined
+mw.use(fn3.bind(context));
+mw(10);
